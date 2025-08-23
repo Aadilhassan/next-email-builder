1c46d18cf57d86a90763e3edb1c127843d44548b
Aadilhassan <aahanalhassan@gmail.com>
2025-08-24 00:54:53 +0530

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+    },
+  ],
+  plugins: [
+    typescript(),
+    babel({
+      presets: ['@babel/preset-react'],
+    }),
+  ],
+  external: ['react', 'react-dom', 'next'],
+};
